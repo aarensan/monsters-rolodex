f1953b6dbc02a0eed0f2e83b54cc5d4d85d84e8f
Aaren San <info@aarensan.com>
2019-09-08 22:29:57 +0200

    Updates

---
+++ precache-manifest.f522c6c1b2150b6de533f900b14acc8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78302d810f975dc8742d67b54e153a97",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "32417518f7189b667a39",
+    "url": "/monsters-rolodex/static/css/main.b3a00a8d.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "32417518f7189b667a39",
+    "url": "/monsters-rolodex/static/js/main.c4892f0b.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
